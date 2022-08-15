a1d553538bb474e6b7cc89dbfecb85a08449982c
HaiYing Wang <whygps@gmail.com>
2022-08-15 17:03:04 -0400

    0.0.8

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 emacs README.org --batch --no-init-file --load run.el -f org-latex-export-to-latex --kill -f toggle-debug-on-error
 
-pdflatex -shell-escape runcode.tex
+# pdflatex -shell-escape runcode.tex
 
 # emacs --batch --no-init-file --load publishWeb.el --funcall toggle-debug-on-error --funcall OS-publish-all
 
