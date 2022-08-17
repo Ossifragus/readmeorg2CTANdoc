8b44a51177a7b66e2d38139638ea3624e8d06e1e
HaiYing Wang <whygps@gmail.com>
2022-08-17 11:11:39 -0400

    add example folder

---
+++ examples/QREMdoc/Sim23.R
@@ -0,0 +1,24 @@
+library(QREM)
+
+##########################################################################
+# Simulation 23 in table A3.
+# Quadratic mean model, increasing variance.
+# Compare the fit of the correct model, y~x+I(x^2), with the incorrect,
+#   linear model. Show diagnostic plots - Q-Q plot when qn=0.1, and
+#   a flat diagnostic plot for all the quantiles, qns.
+set.seed(21322)
+n <- 2000
+qns <- seq(0.05,0.95,by=0.05)
+x <- seq(0,3,length.out = n)
+L <- 20
+y <- 6*x^2 + x +120 + rnorm(n, 0, 0.1+0.5*x)
+qrfits1 <- qrfits2 <- list()
+dat <- data.frame(y,x,x^2)
+for (i in 1:length(qns)) {
+  qrfits1[[i]] <- QREM(lm, linmod=y~x+I(x^2), df=dat, qn=qns[i])
+  qrfits2[[i]] <- QREM(lm, linmod=y~x, df=dat, qn=qns[i])
+}
+qrdg10c <- QRdiagnostics(dat$x, "x", qrfits1[[2]]$ui, qn=0.1, plot.it=TRUE, filename = "tmp/sim23q10correct.pdf")
+qrdg10i <- QRdiagnostics(dat$x, "x", qrfits2[[2]]$ui, qn=0.1, plot.it=TRUE, filename = "tmp/sim23q10incorrect.pdf")
+pvals <- flatQQplot(dat=dat, cnum = 2, qrfits=qrfits1, qns=qns, maxm = 20, plot.it = TRUE, filename = "tmp/flatQQsim23q10correct.pdf")
+pvals <- flatQQplot(dat=dat, cnum = 2, qrfits=qrfits2, qns=qns, maxm = 20, plot.it = TRUE, filename = "tmp/flatQQsim23q10incorrect.pdf")
---
+++ examples/QREMdoc/temperatures.R
@@ -0,0 +1,22 @@
+# quantile regression application - temperature data, Las Vegas, NV
+
+library("QREM")
+library("lme4")
+dat <- read.csv("LasVegas.csv", header=TRUE)
+dat$DATE <- as.Date(dat$DATE, "%Y-%m-%d")
+dat$year <- as.numeric(substring(dat$DATE, 1, 4))
+dat$month <- factor(months(dat$DATE), levels = month.name)
+dat$yday <- as.numeric(format(dat$DATE,"%j"))
+dat$sinedoy <- sin(2*pi*(dat$yday-11)/365-pi/2)
+
+linmodMin <- TMIN ~ sinedoy + year + (1|month)
+linmodMax <- TMAX ~ sinedoy + year + (1|month)
+
+qremFit10min <- QREM(lmer,linmodMin, dat,qn = 0.1)
+qrdg10 <- QRdiagnostics(dat$year, "year (min. temp.)", qremFit10min$ui, qn=0.1, filename = "tmp/LVmintemp.pdf")
+
+qremFit90max <- QREM(lmer,linmodMax, dat,qn = 0.9)
+qrdg90 <- QRdiagnostics(dat$year, "year (max. temp.)", qremFit90max$ui, qn=0.9, filename = "tmp/LVmaxtemp.pdf")
+
+# can also use a generalized additive model (gam), but the fit it slower
+#  gam10fit <- QREM(gam, TMIN ~ s(yday,4) + year + month, data=dat, qn=0.1)
