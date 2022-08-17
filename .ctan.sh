86e5c3cc8e5841ab2bc9fd94b85defa408021aff
HaiYing Wang <whygps@gmail.com>
2022-08-16 21:21:31 -0400

    add 1.7 of runcode main

--- .ctan.sh
+++ .ctan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-tar cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf troubleshoot.tex troubleshoot.pdf ./CTAN/README --transform='flags=r;s|./CTAN/README|README|'
+tar cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf runcode_troubleshoot.tex runcode_troubleshoot.pdf generated README 
 
-# tar  --transform='flags=r;s|./CTAN/README|README|' -cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf troubleshoot.tex troubleshoot.pdf ./CTAN/README
+# rm README header.tex 
+
+# tar cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf troubleshoot.tex troubleshoot.pdf generated ./CTAN/README --transform='flags=r;s|./CTAN/README|README|'
 
 
